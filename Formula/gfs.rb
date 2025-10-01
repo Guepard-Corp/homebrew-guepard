ed6f8153f5b3ba17736ae1624adc9b5190ad6330
Ghassen MENAOUAR <ghassen.menaouar@gmail.com>
2025-10-02 00:51:17 +0200

    Update Linux SHA256 checksums

--- Formula/gfs.rb
+++ Formula/gfs.rb
@@ -17,6 +17,17 @@ class Gfs < Formula
     end
   end
 
+      on_linux do
+        on_arm do
+          url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.25.1/guepard-cli-0.25.1-linux-arm64.tar.gz"
+          sha256 "6d28ecac558b761dbaabd7400343009dc02e0ae2f0e6c86fdea6eece657fa834"
+        end
+        on_intel do
+          url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.25.1/guepard-cli-0.25.1-linux-amd64.tar.gz"
+          sha256 "b6c1f7bd9073dfd49067d3df8d64d22bbd9cd58e815205521b71f1bfbf773514"
+        end
+      end
+
   def install
     bin.install "gfs"
   end
