51e3d7fcbe25677d7b2dd97decac4ebfa03e4b66
Ghassen MENAOUAR <ghassen.menaouar@gmail.com>
2025-10-03 22:44:01 +0200

    Update Guepard formula to version 0.26.19 with revised SHA256 checksums and simplified description

--- Formula/guepard.rb
+++ Formula/guepard.rb
@@ -1,30 +1,26 @@
 class Guepard < Formula
-  desc "Guepard CLI (guepard) - Git, but for Data"
+  desc "Guepard CLI - Git for Data"
   homepage "https://www.guepard.run"
   url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.26.19/guepard-cli-v0.26.19-macos-arm64.tar.gz"
   version "0.26.19"
-  sha256 "568929d4a8ada6e38c9eaa0222eafc946bead757b8fc90a8bb1fd22e111cde69"
+  sha256 "0fad9015e7c702a27431a630bbe957664ad484bfad7d82321e8ba9d69c8defb2"
   license "Guepard (c) 2025"
 
   on_macos do
-    on_arm do
-      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.26.19/guepard-cli-v0.26.19-macos-arm64.tar.gz"
-      sha256 "568929d4a8ada6e38c9eaa0222eafc946bead757b8fc90a8bb1fd22e111cde69"
-    end
     on_intel do
       url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.26.19/guepard-cli-v0.26.19-macos-amd64.tar.gz"
-      sha256 "3322235013aca5021b83d52cce49a1b57e6e7780727428372a411f1c406d89f7"
+      sha256 "e51583ea6966e4a4c7f4af92a798b547186dfcd1b397a8dde6ae0166d3d6cba7"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.26.19/guepard-cli-v0.26.19-linux-arm64.tar.gz"
-      sha256 "e44b5c47f070dcde1f088e4b43ef54f31e9e1acbe1c336e4645b7fcc65a9ffdf"
+      sha256 "3589a31cee62813cb76b530884dde3ab99570de12d1fa255a498cabbaf945fe3"
     end
     on_intel do
       url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.26.19/guepard-cli-v0.26.19-linux-amd64.tar.gz"
-      sha256 "4815b4ab32ab9ed9824577446fd38ad2e0a6a170c672e46e2242c4ea564bf7aa"
+      sha256 "bfee54d8f6ac956618c26373fd6c547b8e3d64cbe87e0185aedeb99d58749ce9"
     end
   end
 
