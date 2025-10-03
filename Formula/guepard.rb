48dfb31df7903f1470deac13f7543ca21e50bfdd
Ghassen MENAOUAR <ghassen.menaouar@gmail.com>
2025-10-03 22:07:49 +0200

    Fix Guepard formula URLs to include version in the filename for consistency across macOS and Linux

--- Formula/guepard.rb
+++ Formula/guepard.rb
@@ -1,29 +1,29 @@
 class Guepard < Formula
   desc "Guepard CLI (guepard) - Git, but for Data"
   homepage "https://www.guepard.run"
-  url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.26.19/guepard-cli-0.26.19-macos-arm64.tar.gz"
+  url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.26.19/guepard-cli-v0.26.19-macos-arm64.tar.gz"
   version "0.26.19"
   sha256 "568929d4a8ada6e38c9eaa0222eafc946bead757b8fc90a8bb1fd22e111cde69"
   license "Guepard (c) 2025"
 
   on_macos do
     on_arm do
-      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.26.19/guepard-cli-0.26.19-macos-arm64.tar.gz"
+      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.26.19/guepard-cli-v0.26.19-macos-arm64.tar.gz"
       sha256 "568929d4a8ada6e38c9eaa0222eafc946bead757b8fc90a8bb1fd22e111cde69"
     end
     on_intel do
-      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.26.19/guepard-cli-0.26.19-macos-amd64.tar.gz"
+      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.26.19/guepard-cli-v0.26.19-macos-amd64.tar.gz"
       sha256 "3322235013aca5021b83d52cce49a1b57e6e7780727428372a411f1c406d89f7"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.26.19/guepard-cli-0.26.19-linux-arm64.tar.gz"
+      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.26.19/guepard-cli-v0.26.19-linux-arm64.tar.gz"
       sha256 "e44b5c47f070dcde1f088e4b43ef54f31e9e1acbe1c336e4645b7fcc65a9ffdf"
     end
     on_intel do
-      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.26.19/guepard-cli-0.26.19-linux-amd64.tar.gz"
+      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.26.19/guepard-cli-v0.26.19-linux-amd64.tar.gz"
       sha256 "4815b4ab32ab9ed9824577446fd38ad2e0a6a170c672e46e2242c4ea564bf7aa"
     end
   end
