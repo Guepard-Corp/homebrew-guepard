4dd6716d4e673c8772502d75be6ee8ab98d8e8ea
Ghassen MENAOUAR <ghassen.menaouar@gmail.com>
2025-10-06 15:03:38 +0200

    Fix Guepard formula URLs to include version in the filename for macOS and Linux binaries

--- Formula/guepard.rb
+++ Formula/guepard.rb
@@ -1,25 +1,26 @@
 class Guepard < Formula
   desc "Guepard CLI - Git for Data"
   homepage "https://www.guepard.run"
-  url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.27.11/guepard-cli-0.27.11-macos-arm64.tar.gz"
+  
+  url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.27.11/guepard-cli-v0.27.11-macos-arm64.tar.gz"
   version "0.27.11"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "Guepard (c) 2025"
 
   on_macos do
     on_intel do
-      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.27.11/guepard-cli-0.27.11-macos-amd64.tar.gz"
+      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.27.11/guepard-cli-v0.27.11-macos-amd64.tar.gz"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.27.11/guepard-cli-0.27.11-linux-arm64.tar.gz"
+      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.27.11/guepard-cli-v0.27.11-linux-arm64.tar.gz"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
     on_intel do
-      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.27.11/guepard-cli-0.27.11-linux-amd64.tar.gz"
+      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.27.11/guepard-cli-v0.27.11-linux-amd64.tar.gz"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
