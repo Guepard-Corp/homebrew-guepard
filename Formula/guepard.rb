494d71b11730d83432690e3890d492e7cc5194e6
Ghassen MENAOUAR <ghassen.menaouar@gmail.com>
2025-10-06 14:49:50 +0200

    Update Guepard formula to version 0.27.11 with fixed URLs and updated SHA256 checksums for macOS and Linux binaries

--- Formula/guepard.rb
+++ Formula/guepard.rb
@@ -1,30 +1,26 @@
 class Guepard < Formula
-  desc "Guepard CLI (guepard) - Git, but for Data"
+  desc "Guepard CLI - Git for Data"
   homepage "https://www.guepard.run"
-  url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v\${VERSION}/guepard-cli-v\${VERSION}-macos-arm64.tar.gz"
-  version "\${VERSION}"
-  sha256 "ce2f2b086481989ad759ae094cf0b4591b130372f0bb8ca38049ac97ca413d8b"
+  url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.27.11/guepard-cli-0.27.11-macos-arm64.tar.gz"
+  version "0.27.11"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "Guepard (c) 2025"
 
   on_macos do
-    on_arm do
-      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v\${VERSION}/guepard-cli-v\${VERSION}-macos-arm64.tar.gz"
-      sha256 "ce2f2b086481989ad759ae094cf0b4591b130372f0bb8ca38049ac97ca413d8b"
-    end
     on_intel do
-      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v\${VERSION}/guepard-cli-v\${VERSION}-macos-amd64.tar.gz"
-      sha256 "7ff94b1d73a8c07b1e4fc9ab9da5f369bcc31bbad6beb2c9db0cb1376e907a4d"
+      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.27.11/guepard-cli-0.27.11-macos-amd64.tar.gz"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v\${VERSION}/guepard-cli-v\${VERSION}-linux-arm64.tar.gz"
-      sha256 "255e017731747a729227d268f76196386c87cd593268aea654a023d3cc78e635"
+      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.27.11/guepard-cli-0.27.11-linux-arm64.tar.gz"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
     on_intel do
-      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v\${VERSION}/guepard-cli-v\${VERSION}-linux-amd64.tar.gz"
-      sha256 "7ef51112d8bf4d82e3ee68d6eb360df78a1f696f149040684212a42d29a12dde"
+      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.27.11/guepard-cli-0.27.11-linux-amd64.tar.gz"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
