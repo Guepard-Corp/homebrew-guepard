bdbb83558b5b83b8d1d211346e8b05fa1b522996
Ghassen MENAOUAR <ghassen.menaouar@gmail.com>
2026-02-23 14:50:48 +0100

    Update Guepard formula to version 0.29.7 with updated URLs and SHA256 checksums for macOS and Linux binaries

--- Formula/guepard.rb
+++ Formula/guepard.rb
@@ -1,26 +1,26 @@
 class Guepard < Formula
   desc "Guepard CLI - Git for Data"
   homepage "https://www.guepard.run"
-  url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.29.6/guepard-cli-0.29.6-macos-arm64.tar.gz"
-  version "0.29.6"
-  sha256 "2768230bd0bb324e23088276437d61c340843e69dd4cb0910f4f5cc830211ac7"
+  url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.29.7/guepard-cli-0.29.7-macos-arm64.tar.gz"
+  version "0.29.7"
+  sha256 "e84247f81bf1184ec5b8c5155e1d643cfe5d3fa3943340afeebf99b7187497ca"
   license "Guepard (c) 2025"
 
   on_macos do
     on_intel do
-      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.29.6/guepard-cli-0.29.6-macos-amd64.tar.gz"
-      sha256 "4599bae0740a5c5bbdbe4c627525b5dc9ee6be383aee67d53947fd2cd462a9dc"
+      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.29.7/guepard-cli-0.29.7-macos-amd64.tar.gz"
+      sha256 "94666c436ec9fe16c81e3c91d5b482cd54696fcf6d578da2d3a43cbea365fce0"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.29.6/guepard-cli-0.29.6-linux-arm64.tar.gz"
-      sha256 "155c41a75e6634de1ed6d5b0c5b440ccf35888b77de100da91b29f7941e20229"
+      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.29.7/guepard-cli-0.29.7-linux-arm64.tar.gz"
+      sha256 "1739e1a2b018019a1e504892369bdd619c7ccfc7bd7056c575af002e0440b83a"
     end
     on_intel do
-      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.29.6/guepard-cli-0.29.6-linux-amd64.tar.gz"
-      sha256 "b286bd2ccb8c3688a15590c84af5f91315cbcf67ae1e2d67acccc2f1c9b668c9"
+      url "https://github.com/Guepard-Corp/guepard-cli/releases/download/v0.29.7/guepard-cli-0.29.7-linux-amd64.tar.gz"
+      sha256 "d8ab45a005258fd5dba175876493344aff3f7df251e7ebac4d018f152904f9ee"
     end
   end
 
